0a435ec8d9f0ec8e6727e697cae4a6b1f448546d
coline <coline-grasset@outlook.fr>
2021-01-08 15:24:39 +0100

    crea class Geno

---
+++ src/AnaVi/Script_génération_carte/classGeno.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Geno:
+    # constructor
+    def __init__(self):
+        # integer : num
+        self.strain = None
+        # integer : is the daughter of..
+        self.daughter = None
+        # String : Genotype
+        self.genotype = None
+
+
+
+    def add_trick(self, trick):
+        self.tricks.append(trick)
