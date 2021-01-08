2ec4d1b22dc7b6f132822365d41dcd290173c476
claire_bouillot <claire.bouillot12@gmail.com>
2021-01-08 15:23:25 +0100

    class Num

---
+++ src/AnaVi/Script_génération_carte/classNum.py
@@ -0,0 +1,14 @@
+class Num:
+
+    #Constructor
+    def __init__(self):
+        # String : EC number of the gene
+        self.ECnum = None
+        # String : modification on the gene
+        self.modif = None
+
+
+    #Function to add the object to the list
+    def add_trick(self, trick):
+        self.trick.append(trick)
+
