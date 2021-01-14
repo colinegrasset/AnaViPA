425821161dbaf7149ff5b9823273072cf7ed668c
coline <coline-grasset@outlook.fr>
2021-01-14 16:15:11 +0100

    Crea_list_Geno.py essais mais pas encore concluant

--- src/AnaVi/Script_génération_carte/Crea_list_Geno.py
+++ src/AnaVi/Script_génération_carte/Crea_list_Geno.py
@@ -3,9 +3,9 @@
 import numpy as np
 import csv
 from AnaVi.Script_génération_carte.classGeno import Geno
-from AnaVi.Script_génération_carte import tablGeno
+#from AnaVi.Script_génération_carte import tablGeno
 
-file = "tablGeno.csv"
+#file = tablGeno
 
 
 ###########################################
@@ -19,7 +19,7 @@ def __repr__(pli):
 
 def csv_tablo_Geno(file):
     csv.register_dialect('myDialect', delimiter=';', quotechar='|')
-    reader = csv.DictReader(open(file, "r"), dialect='myDialect')
+    reader = csv.DictReader(open("tablGeno.csv", "r"), dialect='myDialect')
 
     geno_array = []  # creation d'une liste qui va contenir la class claasGeno x nb de ligne du .csv
 
