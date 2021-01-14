2b01d11e08cbe37bceb1ff335dee622d35f4dd1b
claire_bouillot <claire.bouillot12@gmail.com>
2021-01-14 16:48:05 +0100

    lecture tableau ECnum

--- src/AnaVi/Script_génération_carte/Crea_list_Num.py
+++ src/AnaVi/Script_génération_carte/Crea_list_Num.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python
 # vim: set fileencoding=utf-8 :
 import csv
-import classNum
+from AnaVi.Script_génération_carte.classNum import Num
+
+file="tablECnum.csv"
 
 def __repr__(line):
     return(print ("listNumArray: ECnum({}), modif({})".format(line.ECnum, line.modif)))
@@ -13,12 +15,12 @@ def csv_ECnum (file) :
     listNum_array = [] #create a list which is composed by Num objects
     for row in reader:
         #list pli prendre les valeurs de la class plistruct du package PLIStruct
-        line = classNum.Num()
+        line = Num()
 
         for column, value in row.items():#parcours le fichiers csv valeur par valeur en aller de gauche a droite puis passe a la ligne suivante
-            if(column == "ECnumber") :
+            if(column == "EC number") :
                 line.ECnum = value
-            if(column == "modification") :
+            if(column == "Modification") :
                 line.modif = value
 
         listNum_array.append(line)  # commande qui ajoute une ligne a la liste pliArray
@@ -29,3 +31,5 @@ def csv_ECnum (file) :
         __repr__(line)
 
     return listNum_array
+
+csv_ECnum(file)
