b44c6f645982d69aa5d5c399c51ad20046f390d2
coline <coline-grasset@outlook.fr>
2021-01-14 16:02:46 +0100

    Crea_list_Geno.py essais mais pas encore concluant

--- src/AnaVi/Script_génération_carte/Crea_list_Geno.py
+++ src/AnaVi/Script_génération_carte/Crea_list_Geno.py
@@ -2,17 +2,20 @@
 # vim: set fileencoding=utf-8 :
 import numpy as np
 import csv
+from AnaVi.Script_génération_carte.classGeno import Geno
+from AnaVi.Script_génération_carte import tablGeno
 
 file = "tablGeno.csv"
 
+
 ###########################################
- #   ça marche pas vraiment encore... #
- ##########################################
+#   ça marche pas vraiment encore... #
+##########################################
 
 def __repr__(pli):
     print("pliArray: Strain({}), daughter({}), genotype({}))".format(
         pli.strain, pli.daughter, pli.genotype))
-    return None
+
 
 def csv_tablo_Geno(file):
     csv.register_dialect('myDialect', delimiter=';', quotechar='|')
@@ -21,7 +24,7 @@ def csv_tablo_Geno(file):
     geno_array = []  # creation d'une liste qui va contenir la class claasGeno x nb de ligne du .csv
 
     for row in reader:
-        pli = __repr__.Geno()  # liste pli prendre les valeurs de la class Geno du package classGeno
+        pli = Geno()  # liste pli prendre les valeurs de la class Geno du package classGeno
 
         # parcours le fichiers csv valeur par valeur en aller de gauche a droite puis passe a la ligne suivante
         for column, value in row.items():
@@ -34,7 +37,7 @@ def csv_tablo_Geno(file):
 
             geno_array.append(pli)  # commande qui ajoute un pli a la liste geno_array
 
-    #file.close()
+    # file.close()
 
     for pli in geno_array:
         __repr__(pli)
