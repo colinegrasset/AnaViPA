40fafa96dc4935fd268ecf11c25f072bc66db45e
ltella <lucie.tella@etu.univ-poitiers.fr>
2021-01-21 15:47:57 +0100

    test

--- src/AnaVi/dictionary_mutation.py
+++ src/AnaVi/dictionary_mutation.py
@@ -1,12 +1,14 @@
-from AnaVi.Script_génération_carte import Crea_list_Num, Crea_list_Geno
+from AnaVi.Script_génération_carte import Crea_list_Geno,Crea_list_Num
 from AnaVi.Script_génération_carte.classGeno import Geno
 from AnaVi.Script_génération_carte.classNum import Num
-
+import regex
 
 
 def fillingDict():
-
+    regle = regex.compile(r"(.*)\ (.*)\ (.*)")
     ListGeno = Crea_list_Geno.csv_tablo_Geno("tablGeno.csv")
     ListECnum = Crea_list_Num.csv_ECnum("tablECnum.csv")
     for pli in ListGeno:
         AllGeno = ListGeno.genotype
+        rep = regle.search(AllGeno)
+        print(rep.groups())
