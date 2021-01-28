c730c60c875f750123a983d3b32719cece55f36c
claire_bouillot <claire.bouillot12@gmail.com>
2021-01-28 14:40:20 +0100

    infos legende

--- src/AnaVi/Script_génération_carte/Link_svg.py
+++ src/AnaVi/Script_génération_carte/Link_svg.py
@@ -7,7 +7,7 @@ class LinkSvg:
         # Metabolic map
         self.mapM = None
 
-    def readmap(mapM, idrect):
+    def readmap(mapM, idrect, modification):
         tree = ET.parse(mapM)
         root = tree.getroot()
         for child in root:
@@ -16,9 +16,11 @@ class LinkSvg:
                     if grandchild.tag == "{http://www.w3.org/2000/svg}rect":
                         if grandchild.attrib['id'] == idrect:
                             #print(grandchild.tag, grandchild.attrib['id'])
-                            grandchild.set('style', 'opacity:0.99;fill:#e24620;fill-opacity:0.68032789;stroke-width:0.264583')
+                            grandchild.set('style', modification)
                             tree.write('outputMap.svg')
 
+sousExpression = 'opacity:0.985;fill:none;fill-opacity:0;stroke:#f68c00;stroke-width:0.964999;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1'
+surExpression = 'opacity:0.985;fill:none;fill-opacity:0;stroke:#f61800;stroke-width:0.964999;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1'
 idRect = "2.7.1.12"
 map = "carte_metabolique_pentose-arginine.svg"
-LinkSvg.readmap(map, idRect)
+LinkSvg.readmap(map, idRect, sousExpression)
