9c7c015cec1ee513e914e9046ef4b457f1d6651c
claire_bouillot <claire.bouillot12@gmail.com>
2021-01-28 16:48:44 +0100

    légende

--- src/AnaVi/Script_génération_carte/Link_svg.py
+++ src/AnaVi/Script_génération_carte/Link_svg.py
@@ -11,7 +11,25 @@ class LinkSvg:
         root.insert(4, ET.Element('ns0:rect style="fill:#f9f9f9;fill-opacity:0.26;stroke:#4838bd;stroke-width:6.04724;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect1488-8" width="63.462658" height="31.731329" x="1004.5184" y="196.98962" '))
         root.insert(5, ET.Element('ns0:rect style="fill:#f9f9f9;fill-opacity:0.26;stroke:#ab38bd;stroke-width:6.04724;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect1488-6" width="63.462658" height="31.731329" x="1004.051" y="301.17517" '))
         root.insert(6, ET.Element('ns0:rect style="fill:#f9f9f9;fill-opacity:0.26;stroke:#f61800;stroke-width:6.04724;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect1488-7" width="63.462658" height="31.731329" x="1004.1976" y="249.27013" '))
-        root.insert(6, ET.Element('ns0:rect style="fill:#f9f9f9;fill-opacity:0.26;stroke:#f68c00;stroke-width:6.04724;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect1488-5" width="63.462658" height="31.731329" x="1004.2778" y="144.39932" '))
+        root.insert(7, ET.Element('ns0:rect style="fill:#f9f9f9;fill-opacity:0.26;stroke:#f68c00;stroke-width:6.04724;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1" id="rect1488-5" width="63.462658" height="31.731329" x="1004.2778" y="144.39932" '))
+        #root.insert(8, ET.Element("ns0":"text", xml:space="preserve", style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:24px;line-height:1.25;font-family:sans-serif;-inkscape-font-specification:'sans-serif, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;fill:#000000;fill-opacity:1;stroke:none", x="1082.6063", y="115.92677", id="text1531" "))
+        texte = ET.SubElement(root, "ns0:text", space="preserve", style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:24px;line-height:1.25;font-family:sans-serif;-inkscape-font-specification:'sans-serif, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;fill:#000000;fill-opacity:1;stroke:none", x="1082.6063", y="115.92677", id="text1531")
+        span = ET.SubElement(texte, 'ns0:tspan', role="line", id="tspan1529", x="1082.6063", y="115.92677")
+        span.text = "Sur-expression"
+        texte2 = ET.SubElement(root, "ns0:text", space="preserve", style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:24px;line-height:1.25;font-family:sans-serif;-inkscape-font-specification:'sans-serif, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;fill:#000000;fill-opacity:1;stroke:none", x="1082.6063", y="166.48784", id="text1531-0")
+        span = ET.SubElement(texte2, 'ns0:tspan', role="line", id="tspan1529", x="1082.6063", y="166.48784")
+        span.text = "Sous-expression"
+        texte3 = ET.SubElement(root, "ns0:text", space="preserve", style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:24px;line-height:1.25;font-family:sans-serif;-inkscape-font-specification:'sans-serif, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;fill:#000000;fill-opacity:1;stroke:none", x="1082.6063", y="221.64655", id="text1531")
+        span = ET.SubElement(texte3, 'ns0:tspan', role="line", id="tspan1529", x="1082.6063", y="221.64655")
+        span.text = "Insertion"
+        texte4 = ET.SubElement(root, "ns0:text", space="preserve", style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:24px;line-height:1.25;font-family:sans-serif;-inkscape-font-specification:'sans-serif, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;fill:#000000;fill-opacity:1;stroke:none", x="1082.6063", y="275.87033", id="text1531")
+        span = ET.SubElement(texte4, 'ns0:tspan', role="line", id="tspan1529", x="1082.6063", y="275.87033")
+        span.text = "Délétion"
+        texte5 = ET.SubElement(root, "ns0:text", space="preserve", style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:24px;line-height:1.25;font-family:sans-serif;-inkscape-font-specification:'sans-serif, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;fill:#000000;fill-opacity:1;stroke:none", x="1082.6063", y="325.41968", id="text1531")
+        span = ET.SubElement(texte5, 'ns0:tspan', role="line", id="tspan1529", x="1082.6063", y="325.41968")
+        span.text = "Mutant"
+        span2 = ET.SubElement(span, 'ns0:tspan', style="font-style:italic;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:24px;font-family:sans-serif;-inkscape-font-specification:'sans-serif, Italic';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal", id="tspan1575")
+        span2.text = " fbr"
 
     def readmap(mapM, idrect, modification):
         tree = ET.parse(mapM)
