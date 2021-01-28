b24133b279359d6f566c13d65d848bab81646659
Rachel <rachelvillard74@gmail.com>
2021-01-28 14:37:00 +0100

    couleur légendes

--- src/AnaVi/Script_génération_carte/Link_svg.py
+++ src/AnaVi/Script_génération_carte/Link_svg.py
@@ -14,11 +14,18 @@ class LinkSvg:
             if child.tag == "{http://www.w3.org/2000/svg}g":
                 for grandchild in child:
                     if grandchild.tag == "{http://www.w3.org/2000/svg}rect":
+
                         if grandchild.attrib['id'] == idrect:
                             print(grandchild.tag, grandchild.attrib['id'])
-                            grandchild.set('style', 'opacity:0.99;fill:#e24620;fill-opacity:0.68032789;stroke-width:0.264583')
+                            grandchild.set('style', vert)
                             tree.write('outputMap.svg')
 
 idRect = "2.7.1.12"
 map = "carte_metabolique_pentose-arginine.svg"
+
+
+violet = 'fill:#f9f9f9;fill-opacity:0.26;stroke-width:0.965;stroke-miterlimit:4;stroke-dasharray:none;stroke:#ab38bd;stroke-opacity:1'
+bleu = 'fill:#f9f9f9;fill-opacity:0.26;stroke-width:0.965;stroke-miterlimit:4;stroke-dasharray:none;stroke:#4838bd;stroke-opacity:1'
+vert = 'fill:#f9f9f9;fill-opacity:0.26;stroke-width:1.365;stroke-miterlimit:4;stroke-dasharray:none;stroke:#22d327;stroke-opacity:1'
+
 LinkSvg.readmap(map, idRect)
