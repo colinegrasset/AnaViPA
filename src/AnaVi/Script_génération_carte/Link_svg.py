4d346c1bab1b52e7bf423104e71071b9fece3e26
claire_bouillot <claire.bouillot12@gmail.com>
2021-02-05 11:05:37 +0100

    modif couleurs légende

--- src/AnaVi/Script_génération_carte/Link_svg.py
+++ src/AnaVi/Script_génération_carte/Link_svg.py
@@ -4,11 +4,11 @@ import re
 
 
 def legende(root):
-    ET.SubElement(root, 'ns0:rect', style="fill:#f9f9f9;fill-opacity:0.26;stroke:#09a001;stroke-width:5.15906", id="rect1488", width="63.462658", height="31.731329", x="1003.5033", y="-124.28104", transform="scale(1,-1)")
-    ET.SubElement(root, 'ns0:rect', style="fill:#f9f9f9;fill-opacity:0.26;stroke:#4838bd;stroke-width:6.04724;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1", id="rect1488-8", width="63.462658", height="31.731329", x="1004.5184", y="196.98962")
-    ET.SubElement(root, 'ns0:rect', style="fill:#f9f9f9;fill-opacity:0.26;stroke:#ab38bd;stroke-width:6.04724;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1", id="rect1488-6", width="63.462658", height="31.731329", x="1004.051", y="301.17517")
-    ET.SubElement(root, 'ns0:rect', style="fill:#f9f9f9;fill-opacity:0.26;stroke:#f61800;stroke-width:6.04724;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1", id="rect1488-7", width="63.462658", height="31.731329", x="1004.1976", y="249.27013")
-    ET.SubElement(root, 'ns0:rect', style="fill:#f9f9f9;fill-opacity:0.26;stroke:#f68c00;stroke-width:6.04724;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1", id="rect1488-5", width="63.462658", height="31.731329", x="1004.2778", y="144.39932")
+    ET.SubElement(root, 'ns0:rect', style="fill:#f9f9f9;fill-opacity:0.26;stroke-width:7.55905512;stroke-miterlimit:4;stroke-dasharray:none;stroke:#0ea139;stroke-opacity:1", id="rect1488", width="63.462658", height="31.731329", x="1003.5033", y="-124.28104", transform="scale(1,-1)")
+    ET.SubElement(root, 'ns0:rect', style="fill:#f9f9f9;fill-opacity:0.26;stroke-width:7.55905512;stroke-miterlimit:4;stroke-dasharray:none;stroke:#009fe3;stroke-opacity:1", id="rect1488-8", width="63.462658", height="31.731329", x="1004.5184", y="196.98962")
+    ET.SubElement(root, 'ns0:rect', style="fill:#f9f9f9;fill-opacity:0.26;stroke:#ab38bd;stroke-width:7.55905512;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1", id="rect1488-6", width="63.462658", height="31.731329", x="1004.051", y="301.17517")
+    ET.SubElement(root, 'ns0:rect', style="fill:#f9f9f9;fill-opacity:0.26;stroke:#f61800;stroke-width:7.55905512;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1", id="rect1488-7", width="63.462658", height="31.731329", x="1004.1976", y="249.27013")
+    ET.SubElement(root, 'ns0:rect', style="fill:#f9f9f9;fill-opacity:0.26;stroke:#f7a600;stroke-width:7.55905512;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1", id="rect1488-5", width="63.462658", height="31.731329", x="1004.2778", y="144.39932")
     texte = ET.SubElement(root, "ns0:text", space="preserve", style="font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:24px;line-height:1.25;font-family:sans-serif;-inkscape-font-specification:'sans-serif, Normal';font-variant-ligatures:normal;font-variant-caps:normal;font-variant-numeric:normal;font-variant-east-asian:normal;fill:#000000;fill-opacity:1;stroke:none", x="1082.6063", y="115.92677", id="text1531")
     span = ET.SubElement(texte, 'ns0:tspan', role="line", id="tspan1529", x="1082.6063", y="115.92677")
     span.text = "Sur-expression"
@@ -68,11 +68,11 @@ def readmap(mapM, dict):
                 tree.write('outputMap.svg')
 
 # Rectangle styles on the map according to the modification type
-sousExpression = 'fill:#f9f9f9;fill-opacity:0.26;stroke:#f68c00;stroke-width:1.600;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1'
-deletion = 'fill:#f9f9f9;fill-opacity:0.26;stroke:#f61800;stroke-width:1.600;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1'
-mutantFbr = 'fill:#f9f9f9;fill-opacity:0.26;stroke-width:1.600;stroke-miterlimit:4;stroke-dasharray:none;stroke:#ab38bd;stroke-opacity:1'
-insertion = 'fill:#f9f9f9;fill-opacity:0.26;stroke-width:1.600;stroke-miterlimit:4;stroke-dasharray:none;stroke:#4838bd;stroke-opacity:1'
-surExpression = 'fill:#f9f9f9;fill-opacity:0.26;stroke-width:1.600;stroke-miterlimit:4;stroke-dasharray:none;stroke:#22d327;stroke-opacity:1'
+sousExpression = 'fill:#f9f9f9;fill-opacity:0.26;stroke:#f7a600;stroke-width:3;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1'
+deletion = 'fill:#f9f9f9;fill-opacity:0.26;stroke:#f61800;stroke-width:3;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1'
+mutantFbr = 'fill:#f9f9f9;fill-opacity:0.26;stroke-width:3;stroke-miterlimit:4;stroke-dasharray:none;stroke:#ab38bd;stroke-opacity:1'
+insertion = 'fill:#f9f9f9;fill-opacity:0.26;stroke:#009fe3;stroke-width:3;stroke-miterlimit:4;stroke-dasharray:none;stroke-opacity:1;opacity:1'
+surExpression = 'fill:#f9f9f9;fill-opacity:0.26;stroke-width:3;stroke-miterlimit:4;stroke-dasharray:none;stroke:#0ea139;stroke-opacity:1'
 
 
 
