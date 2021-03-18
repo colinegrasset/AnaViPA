bb8ed415133e0b181fe7fe7918e69859c075e555
claire_bouillot <claire.bouillot12@gmail.com>
2021-03-18 15:41:32 +0100

    barplot : màj

--- src/AnaVi/Script_génération_carte/Histogramme.py
+++ src/AnaVi/Script_génération_carte/Histogramme.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 
-def barplot(listSouche):
+def barplot(listSouche,numSouche):
     fig = plt.figure()
     plt.bar(range(3), listSouche)
+    plt.xticks(range(3), ['DO 6h','DO 24h','DO Production'])
+    plt.title('DO souche '+str(numSouche))
     fig.savefig('barplot.png', dpi=fig.dpi)
 
 liste = [12.5,3.5,6.8]
-barplot(liste)
+barplot(liste,12)
